9dae8750d242ee29f079a75c4e01d3d28451390b
Maria Carolina <demycar24@gmail.com>
2022-07-18 14:43:48 -0300

    Vai na Web-Array

---
+++ array/script.js
@@ -0,0 +1,24 @@
+// Crie um array que receba 5 itens e exiba no console.
+let frutas= ['banana','uva', 'caqui','mamão','abacaxi']
+console.log(frutas)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+frutas.unshift ('uva')
+console.log ('frutas')
+
+// Utilize um método para remover o último nome do array.
+frutas.pop ()
+console.log ('frutas')
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+frutas.push ('amora' ,'acerola')
+console.log (frutas)
+
+// Utilize um método para remover o primeiro nome do array.
+frutas.shift ()
+console.log (frutas)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log (numbers)
